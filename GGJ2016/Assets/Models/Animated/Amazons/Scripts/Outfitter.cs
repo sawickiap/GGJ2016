42d84a33e234636e06e1f6eda4ca3f4910514260
zygmuntix <a@a.pl>
2016-01-30 15:05:56 +0100

    Added models.

---
+++ GGJ2016/Assets/Models/Animated/Amazons/Scripts/Outfitter.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+[Serializable]
+public class Outfitter : MonoBehaviour 
+{
+	
+	CharacterDemoController ac;
+	int oldWeaponIndex;
+	[SerializeField]
+	public List<WeaponSlot> weapons;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		ac = GetComponentInChildren<CharacterDemoController>();
+
+        int weaponStateInt = (int)ac.characterWeaponState;
+
+		for(int i = 0;i<weapons.Count;i++)
+		{
+			for(int model=0;model<weapons[i].models.Count;model++)
+			{
+				weapons[i].models[model].enabled = false;
+			}
+		}
+        for (int model = 0; model < weapons[weaponStateInt].models.Count; model++)
+		{
+            weapons[weaponStateInt].models[model].enabled = true;
+		}
+        oldWeaponIndex = weaponStateInt;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+        int weaponStateInt = (int)ac.characterWeaponState;
+
+        if (weaponStateInt != oldWeaponIndex)
+		{
+			for(int model=0;model<weapons[oldWeaponIndex].models.Count;model++)
+			{
+				weapons[oldWeaponIndex].models[model].enabled = false;
+			}
+            for (int model = 0; model < weapons[weaponStateInt].models.Count; model++)
+			{
+                weapons[weaponStateInt].models[model].enabled = true;
+			}
+            oldWeaponIndex = weaponStateInt;
+		}
+	}
+}
+[Serializable]
+public class WeaponSlot
+{
+	[SerializeField]
+	public List<Renderer> models = new List<Renderer>();
+}
---
+++ GGJ2016/Assets/Models/Animated/Scripts/EnemyController.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Models.Animated.Scripts
+{
+    class EnemyController : MonoBehaviour
+    {
+
+        public bool attacking;
+        public bool dead;
+        public bool idle;
+        public float speed;
+        public bool takingDamage;
+        public bool knockback;
+
+
+        private Animator animator;
+
+        // Use this for initialization
+        void Start()
+        {
+
+            speed = 0.0f;
+            dead = false;
+            takingDamage = false;
+            knockback = false;
+            attacking = false;
+            idle = true;
+
+            animator = GetComponentInChildren<Animator>();
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Speed = 1.5f;
+            }
+
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                Attacking = true;
+            }
+
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                Dead = true;
+            }
+
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                TakingDamage = true;
+            }
+
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                Knockback = true;
+            }
+
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                Idle = true;
+            }
+
+        }
+
+
+        public float Speed
+        {
+            get
+            {
+                return speed;
+            }
+            set
+            {
+                this.speed = value;
+                animator.SetFloat("Speed", speed);
+            }
+        }
+
+        public bool Attacking
+        {
+            get
+            {
+                return attacking;
+            }
+            set
+            {
+
+                this.attacking = value;
+                if (attacking)
+                {
+                    animator.SetTrigger("Attacking");
+                }
+
+            }
+        }
+
+        public bool Dead
+        {
+            get
+            {
+                return dead;
+            }
+            set
+            {
+
+                this.dead = value;
+                if (dead)
+                {
+                    animator.SetTrigger("Dead");
+                }
+
+            }
+        }
+
+        public bool Idle
+        {
+            get
+            {
+                return idle;
+            }
+            set
+            {
+
+                this.idle = value;
+                if (idle)
+                {
+                    animator.SetTrigger("Idle");
+                }
+
+            }
+        }
+
+        public bool Knockback
+        {
+            get
+            {
+                return knockback;
+            }
+            set
+            {
+
+                this.knockback = value;
+                if (knockback)
+                {
+                    animator.SetTrigger("Knockback");
+                }
+
+            }
+        }
+
+        public bool TakingDamage
+        {
+            get
+            {
+                return takingDamage;
+            }
+            set
+            {
+
+                this.takingDamage = value;
+                if (takingDamage)
+                {
+                    animator.SetTrigger("TakingDamage");
+                }
+
+            }
+        }
+
+
+    }
+}
