24d476e1a1cec1d49fa9a59b58e6afab43fbab09
Karol Badowski <karolbadowski@gmail.com>
2016-01-30 21:49:52 +0100

    .

--- GGJ2016/Assets/Pole.cs
+++ GGJ2016/Assets/Pole.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class Pole : MonoBehaviour {
 
     public Platforma platforma;
+	public bool onPyramid = true;
+
     public Pole nastepnik;
     public bool naSkrzyzowaniu;
     public int nrSkrzyzowania;
@@ -12,32 +14,35 @@ public class Pole : MonoBehaviour {
 
     public Pole GetNastepnik()
     {
-        if (this.poleNaWieze) return null;
-        else if (this.naSkrzyzowaniu)//platformy bez shodow i nie zerowa
-        {
-            int myplatformID = this.platforma.ID;
-            Platforma kolejnaPlatforma = Piramida.singleton.platformy[myplatformID - 1];
-            if (kolejnaPlatforma.rotacja % 2 != nrSkrzyzowania % 2) return nastepnik;
-            else
-            {
-                if (kolejnaPlatforma.rotacja == this.nrSkrzyzowania) return kolejnaPlatforma.schody1;
-                else return kolejnaPlatforma.schody2;
-            }
+		if(onPyramid){
+	        if (this.poleNaWieze) return null;
+	        else if (this.naSkrzyzowaniu)//platformy bez shodow i nie zerowa
+	        {
+	            int myplatformID = this.platforma.ID;
+	            Platforma kolejnaPlatforma = Piramida.singleton.platformy[myplatformID - 1];
+	            if (kolejnaPlatforma.rotacja % 2 != nrSkrzyzowania % 2) return nastepnik;
+	            else
+	            {
+	                if (kolejnaPlatforma.rotacja == this.nrSkrzyzowania) return kolejnaPlatforma.schody1;
+	                else return kolejnaPlatforma.schody2;
+	            }
 
-        }
-        else if (this.platforma.ID % 2 == 0) return this.nastepnik; //platformy bez schodow
-        else
-        {
-            int nastepnePoleCentralneID = 0;
-            //jesli jestem schodkiem 1
-            if (this == this.platforma.schody1)
-                nastepnePoleCentralneID = this.platforma.rotacja;
-            //jesli jestem schodkiem 2
-            else if (this == this.platforma.schody2)
-                nastepnePoleCentralneID = (this.platforma.rotacja + 2) % 4;
+	        }
+			else if (this.platforma.ID % 2 == 1 || this.platforma.ID == 0) return this.nastepnik; //platformy bez schodow
+	        else
+	        {
+	            int nastepnePoleCentralneID = 0;
+	            //jesli jestem schodkiem 1
+	            if (this == this.platforma.schody1)
+	                nastepnePoleCentralneID = this.platforma.rotacja;
+	            //jesli jestem schodkiem 2
+	            else if (this == this.platforma.schody2)
+	                nastepnePoleCentralneID = (this.platforma.rotacja + 2) % 4;
 
-            return Piramida.singleton.platformy[this.platforma.ID - 1].polaCentralneScian[nastepnePoleCentralneID];
-        }
+	            return Piramida.singleton.platformy[this.platforma.ID - 1].polaCentralneScian[nastepnePoleCentralneID];
+	        }
+		}
+		else return nastepnik;
 
     }
 
