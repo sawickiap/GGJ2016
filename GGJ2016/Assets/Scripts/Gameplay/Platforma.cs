a6c025d0f1759e959fbe6945d73a9ed88ed2ecef
zygmuntix <a@a.pl>
2016-02-01 13:26:49 +0100

    Moved all scripts and shaders to Scripts folder.

---
+++ GGJ2016/Assets/Scripts/Gameplay/Platforma.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Platforma : MonoBehaviour
+{
+
+    public Pole[] fields;
+
+    public int ID;
+    public Pole schody1;//dla id nieparzystych
+    public Pole schody2;//dla id nieparzystych
+
+    const float RotationDuration = 0.5f;
+
+    public Pole[] polaCentralneScian;// dla id nparzystych
+
+    public int rotacja;
+
+    private int lastRotacja;
+    private bool isRotating = false;
+    private Quaternion targetRotation;
+    private Quaternion sourceRotation;
+    private float rotationStartTime;
+
+
+    public Platforma()
+    {
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        fields = GetComponents<Pole>();
+        lastRotacja = rotacja;
+	}
+
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (rotacja != lastRotacja)
+        {
+
+            sourceRotation = transform.localRotation;
+            targetRotation = Quaternion.AngleAxis(90f * rotacja, Vector3.up);
+            rotationStartTime = Time.time;
+            isRotating = true;
+            lastRotacja = rotacja;
+
+        }
+
+        if (isRotating)
+        {
+
+            float rotationProgress = Mathf.Clamp01((Time.time - rotationStartTime) / RotationDuration);
+            rotationProgress = Mathf.SmoothStep(0f, 1f, rotationProgress);
+            transform.localRotation = Quaternion.Slerp(sourceRotation, targetRotation, rotationProgress);
+            if (rotationProgress == 1f)
+                isRotating = false;
+
+        }
+
+    }
+}
